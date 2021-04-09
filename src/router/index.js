b837549408b173f722ff25cc9224fd4c5a434596
Dang Minh Quang <dangminhquang.r@gmail.com>
2021-04-09 15:53:27 +0700

    Change favicon and default page title

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 
+const DEFAULT_TITLE = "Blogr - A modern publishing platform";
 const routes = [
   {
     path: "/",
@@ -15,12 +16,17 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    meta: { title: () => "Blogr | About us" },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.afterEach((to) => {
+  document.title = to.meta.title ? to.meta.title() : DEFAULT_TITLE;
 });
 
 export default router;
